c7fe166a067fded4091670f55c40626719fe2ec0
2571138262 <2571138262@qq.com>
2020-06-23 22:46:22 +0800

    TraceId 实现

--- src/main/java/com/baixiaowen/alllearning/AllLearningApplication.java
+++ src/main/java/com/baixiaowen/alllearning/AllLearningApplication.java
@@ -2,8 +2,10 @@ package com.baixiaowen.alllearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 用来标示我们自定义的filter过滤器可以被SpringBoot扫描到
 public class AllLearningApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/baixiaowen/alllearning/filter/TraceIdFilter.java
+++ src/main/java/com/baixiaowen/alllearning/filter/TraceIdFilter.java
@@ -1,18 +1,42 @@
 package com.baixiaowen.alllearning.filter;
 
+import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * TraceId过滤器
  */
-@Order(1)
+@WebFilter(urlPatterns = "/*") // 过滤所有的请求
+@Order(1)// 设置filter等级
 public class TraceIdFilter implements Filter {
+
+    /**
+     * TraceId常量
+     */
+    private static final String TRACE_ID = "traceId";
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 尝试从请求信息中获取TraceId信息
+        String traceId = servletRequest.getParameter(TRACE_ID);
 
+        // 为空设置默认值
+        if (StringUtils.isEmpty(traceId)){
+            traceId = UUID.randomUUID().toString();
+        }
+
+        // 在MDC中放入traceId
+        // MDC 是日志框架为我们提供的ThreadLocal对象
+        MDC.put(TRACE_ID, traceId);
+
+        filterChain.doFilter(servletRequest, servletResponse);
     }
+
+
 }
